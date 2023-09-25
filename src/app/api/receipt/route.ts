ed1ac51afa878c0eb2facb2efa176bb7059f6f52
hirdbluebird <bluebirdyourdestiny@gmail.com>
2023-09-25 18:08:09 +0200

    design improvments

--- src/app/api/receipt/route.ts
+++ src/app/api/receipt/route.ts
@@ -2,14 +2,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Client, product } from "mindee";
 import { MongoClient } from "mongodb";
-import 'dotenv/config'
+import "dotenv/config";
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.m9byflm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
-
 const mindeeClient = new Client({ apiKey: process.env.MINDEE_API_KEY });
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  // if (process.env.NODE_ENV === "development") {
+  //   return NextResponse.json({ text: "everything is good" });
+  // }
   const file = (await req.formData()).get("photo");
   const a = await file.arrayBuffer();
   const b = Buffer.from(a).toString("base64");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,64 @@ import { FormEvent, useState } from "react";
 
 export default function Home() {
   const [data, setData] = useState();
+  const [uploadInProgress, setUploadInProgress] = useState(false);
+  const [receiptSource, setReceiptSource] = useState("camera");
+
   const submit = async (e: FormEvent<HTMLFormElement>) => {
+    setUploadInProgress(true);
     e.preventDefault();
-    e.stopPropagation();
     const formData = new FormData(e.currentTarget);
-    console.log(formData);
     const response = await fetch("/api/receipt", {
       method: "POST",
       body: formData,
     });
     const data = await response.json();
     setData(data);
+    setUploadInProgress(false);
   };
 
   return (
     <main className={styles.main}>
+      {uploadInProgress && <div className="loader" />}
       <div className={styles.center}>
         <form onSubmit={submit}>
-          <input type="file" accept="image/*" capture name="photo" />
-          <button type="submit">Send image</button>
+          <p>
+            <label htmlFor="camera">
+              <input
+                id="camera"
+                type="radio"
+                name="receiptSource"
+                checked={receiptSource === "camera"}
+                onClick={() => setReceiptSource("camera")}
+              />
+              <span> Take a photo </span>
+            </label>
+          </p>
+          <p>
+            <label htmlFor="file">
+              <input
+                id="file"
+                type="radio"
+                name="receiptSource"
+                checked={receiptSource === "file"}
+                onClick={() => setReceiptSource("file")}
+              />
+              <span> Upload file </span>
+            </label>
+          </p>
+          <input
+            type="file"
+            accept="image/*"
+            capture={receiptSource === "camera"}
+            checked={receiptSource === "camera"}
+            name="photo"
+          />
+          <button type="submit" disabled={uploadInProgress}>
+            Send image
+          </button>
         </form>
       </div>
-      {data && JSON.stringify(data, null, 2)}
+      <pre>{data && JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
